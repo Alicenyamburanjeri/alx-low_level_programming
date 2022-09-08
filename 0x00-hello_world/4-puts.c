a9edd855eeaeb6de98663a9ab6077411169a5cc8
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-08 14:15:24 -0700

    print f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,5 @@
 **/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-
-/**
-*main - print the string in the put function
-*
-*Description: using the main function
-*this program prints "with proper grammar, but the outcome is a piece of art,"
-*Return :0
-*/
-int main(void)
-{
-    printf("with proper grammar, but the outcome is a piece of art, \n");
-    return (0);
+#include <stdio.h>                                
+/**                                               
+ * main - entry point                             
+ *
+ *    
+ * Return: Always 0 (success)                     
+ *
+ **/          
+int main(void)                                    
+{                                                 
+ printf("with proper grammar, but the outcome is a piece of art,\n");    
+ return (0);         
 }
