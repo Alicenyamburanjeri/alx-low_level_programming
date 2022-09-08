0f29606cbc332191b5124a4364d4325ca52aec4d
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-08 12:54:01 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,7 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("
-
-
-
-			 \"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0)
 			 
 }
