69aeb15739cf6067c9757b25c2350f0d7378fba1
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-09 13:00:29 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase letters
+ * except q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && != 'q')
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
