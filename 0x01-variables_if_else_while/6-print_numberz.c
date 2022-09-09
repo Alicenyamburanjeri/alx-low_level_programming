d75526270b193c169982bb58297c98ef66f736cc
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-09 13:27:58 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints digits of
+ * base 10 from 0
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
