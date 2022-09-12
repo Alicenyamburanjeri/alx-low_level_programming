3bb888f72dd0af61f0cca9939790643726588a73
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-12 13:20:34 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
-ii
+#include <stdio.h>
+
+/**
+ * main - Prints putchar followed by
+ * a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
