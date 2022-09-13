ab23eb12756f143b741ae763a3bcfaaf21659854
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-13 14:06:19 -0700

    t.tble

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * time_table - prints time table
+ *
+ * Return: 0
+ */
+void time_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j == 0)
+				_putchar('0');
+			else if (result >= 10)
+			{
+				_putchar(' ');
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
