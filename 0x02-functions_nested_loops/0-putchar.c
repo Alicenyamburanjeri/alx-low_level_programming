6f8acfcd1d671257911e1a720e2e0e3bb867fc23
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-13 02:29:18 -0700

    looping

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-	char c[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(c[i]);
-		
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
