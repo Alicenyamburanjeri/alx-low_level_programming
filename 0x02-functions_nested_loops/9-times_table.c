f301b02009ca0b4de9c426cdf2c88150be2aef60
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-13 14:20:14 -0700

    t.t

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,11 @@ void times_table(void)
 			}
 			if (j == 0)
 				_putchar('0');
-			else if (result >= 10)
+			else if (result >= 10){
+				_putchar((result / 10) + '0');
+				_putchar(result % 10 + '0');
+			}
+			else if ((result < 10) && (j != 0))
 			{
 				_putchar(' ');
 				_putchar((result % 10) + '0');
