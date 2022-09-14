a9684cf16fc53e803514b8f113e00fffab855856
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-13 22:01:15 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers upto 98
+ * @n: printed number
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	}
+	printf("98\n");
+}
