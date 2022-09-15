96bd7107f44534b4376ed08ed4f8a8485da13a74
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-15 00:09:47 -0700

    DBugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,23 +2,23 @@
 
 /**
  * main - causes an infinite loop
- * \\o/ and :( - smilies
+ * \\o/ - smily
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	printf("Infinite loop Incoming :(\n"); /* :( - smily */
+	printf("Infinite loop Incoming :(\n");
 
 	i = 0;
-/**
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
+/*
+* while (i < 10)
+* {
+* putchar(i);
+* }
 */
-	printf("Infinite loop avoided! \\o/\n"); /*\\o/ - smily*/
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
