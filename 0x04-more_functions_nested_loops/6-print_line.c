a0a4af7399bbef800f4029d43d642984e6cbad6b
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-16 01:21:30 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: numbers of characters to be printed
+ * Return: 0
+ */
+
+void print_line(int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -20,4 +20,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void print_line(int n);
+
 #endif /* _MAIN_H */
