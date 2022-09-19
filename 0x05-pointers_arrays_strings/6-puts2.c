aaf6d1a7e5cd3808e6f33ebba4d3fe6371c1e57d
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-19 04:08:45 -0700

    print string char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#incude "main.h"
+
+/**
+ * puts2 - prints characters
+ * @str: pointer to char
+ * char: data type
+ * Return: 0
+ */
+void puts2(char *str);
+{
+	int j;
+
+	for (j = 0; *(str + j) != '\0'; j++)
+	{
+		if (j % 2 == 0)
+			_putchar(*(str + j));
+	}
+	_putchar('\n');
+}
