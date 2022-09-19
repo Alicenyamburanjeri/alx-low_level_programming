cfcbaed2d80bed15ad561ba1b1bea4f3f4ef5c47
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-19 05:43:46 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,12 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int j;
 
-	while (str[i] != '\0')
+	for (j = 0; *(str + j) != '\0'; j++)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		i++;
+		if (j % 2 == 0)
+			_putchar(*(str + j));
 	}
 	_putchar('\n');
 }
