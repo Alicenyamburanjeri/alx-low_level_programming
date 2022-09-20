2a2e7ee249753eca49d80a423b24dd2f1401252a
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-20 03:51:05 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - creates a password
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int keynumber, symbol, checksum;
+
+	srand(time(NULL));
+	checksum = 2772;
+	keynumber = 0;
+
+	while (keynumber < (checksum - 122))
+	{
+		symbol = (rand() % (122 - 97 + 1)) + 97;
+		printf("%c", symbol);
+	}
+	symbol = checksum - keynumber;
+	printf("%c", symbol);
+	return (0);
+}
