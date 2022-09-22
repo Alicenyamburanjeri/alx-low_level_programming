f7af342b0f88c77ebf3a9eee09fbe8b0954a59b9
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-09-22 02:35:15 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - changes a string to 1337 format
+ * @s: char pointer
+ * Return: 0
+ */
+
+char *leet(char *s)
+{
+	char alph[5][2] = {{'a', 'A'}, {'e', 'E'},
+		{'o', 'O'}, {'t', 'T'}, {'l', 'L'}};
+	char nums[5] = {'4', '3', '0', '7', '1'};
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (alph[j][0] == s[i] || alph[j][1] == s[i])
+				s[i] = nums[j];
+		}
+		i++;
+	}
+	return (s);
+}
