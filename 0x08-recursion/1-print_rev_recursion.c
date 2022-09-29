0b7b8ec112457998262b5f5f748e7fbd22a03628
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2022-09-29 16:51:46 +0300

    print rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev_recursion - prints a string in reverse
+ *
+ * @s: char pointer
+ *
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s == 0)
+{
+return;
+}
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
