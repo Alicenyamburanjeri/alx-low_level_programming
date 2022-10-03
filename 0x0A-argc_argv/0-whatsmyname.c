54ddf3d688919919c0becad7ee2227a868d013ab
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-10-03 02:55:26 -0700

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name
+ * @argc: arg counter for char
+ * @argv: arg value for char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = argc - arg c;
+
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
