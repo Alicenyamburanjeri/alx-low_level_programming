6da0bbbff4c4c330365853b88a17a1f203c514ff
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-10-05 04:17:04 -0700

    rm

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-<<<<<<< HEAD
-gcc -c ./*.c
-ar -rc liball.a ./*.o
-=======
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -crs liball.a *.o
->>>>>>> 541acbebf774f3283784335495d080035b099aad
