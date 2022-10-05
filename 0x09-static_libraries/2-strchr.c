541acbebf774f3283784335495d080035b099aad
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2022-10-05 12:44:19 +0300

    libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ *
+ * @s: is a pointer to a char
+ * @c: is a pointer to a char
+ *
+ * Return: Return the transformed pointer
+ *
+**/
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+s++;
+}
