17a31d8dcc0a60533d231caa8b56d13cb38bbc82
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2022-10-15 21:10:20 +0300

    h

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,53 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_all - prints anything
- * @format: any variable type
- * Return: Nothing
-**/
+ * print_all - Entry Point
+ * c = char, i = int, f = float, s = char * (if null print (nil))
+ * @format: list of arg types
+ * Return: 0
+ */
 
 void print_all(const char * const format, ...)
 {
-unsigned int i = 0, j = 0;
-char *str;
-va_list ls;
-va_start(ls, format);
+	va_list valist;
+	int n = 0, i = 0;
+	char *sep = ", ";
+	char *str;
 
-while (format && format[i])
-{
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(ls, int));
-break;
-case 'i':
-printf("%i", va_arg(ls, int));
-break;
-case 'f':
-printf("%f", va_arg(ls, double));
-break;
-case 's':
-str = va_arg(ls, char *);
-if (str == NULL)
-str = "(nil)";
+	va_start(valist, format);
 
-printf("%s", str);
-break;
-}
-j = i + 1;
+	while (format && format[i])
+		i++;
 
-while (format[j] && (format[j] == 'c' || format[j] == 'i' ||
-format[j] == 'f' || format[j] == 's'))
-{
-printf(", ");
-break;
-}
-
-i++;
-}
-printf("\n");
-va_end(ls);
+	while (format && format[n])
+	{
+		if (n  == (i - 1))
+		{
+			sep = "";
+		}
+		switch (format[n])
+		{
+			case 'c':
+				printf("%c%s", va_arg(valist, int), sep);
+				break;
+			case 'i':
+				printf("%d%s", va_arg(valist, int), sep);
+				break;
+			case 'f':
+				printf("%f%s", va_arg(valist, double), sep);
+				break;
+			case 's':
+				str = va_arg(valist, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", str, sep);
+				break;
+		}
+		n++;
+	}
+	printf("\n");
+	va_end(valist);
 }
