2efde842b16d08f3d1d7af2d50d5790ae0b41c6c
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2022-10-15 21:49:10 +0300

    prnt

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,7 @@ void print_all(const char * const format, ...)
 {
 
 	va_list valist;
-	int n = 0, i = 0;
+	unsigned int n = 0, i = 0;
 	char *sep = ", ";
 	char *str;
 
@@ -51,43 +51,4 @@ void print_all(const char * const format, ...)
 	}
 	printf("\n");
 	va_end(valist);
-	unsigned int i = 0, j = 0;
-	char *str;
-
-	va_list ls;
-
-	va_start(ls, format);
-
-	while (format && format[i])
-	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(ls, int));
-				break;
-			case 'i':
-				printf("%i", va_arg(ls, int));
-				break;
-			case 'f':
-
-				printf("%f", va_arg(ls, double));
-				break;
-			case 's':
-				str = va_arg(ls, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s", str);
-				break;
-		}
-		j = i + 1;
-		while (format[j] && (format[j] == 'c' ||
-					format[j] == 'i' || format[j] == 'f' || format[j] == 's'))
-		{
-			printf(", ");
-			break;
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(ls);
 }
