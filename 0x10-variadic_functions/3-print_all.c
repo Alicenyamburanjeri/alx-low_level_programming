46bd9b225c8334a4b8e0d532692ee4698e7053e2
Alicenyamburanjeri <alcenyamburanjeri@gmail.com>
2022-10-15 11:01:21 -0700

    variadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,7 +7,6 @@
  * print_all - prints anything
  * @format: any variable type
  * Return: Nothing
-<<<<<<< HEAD
  */
 
 void print_all(const char * const format, ...)
@@ -51,49 +50,4 @@ void print_all(const char * const format, ...)
 	}
 	printf("\n");
 	va_end(ls);
-=======
-**/
-
-void print_all(const char * const format, ...)
-{
-unsigned int i = 0, j = 0;
-char *str;
-va_list ls;
-va_start(ls, format);
-
-while (format && format[i])
-{
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(ls, int));
-break;
-case 'i':
-printf("%i", va_arg(ls, int));
-break;
-case 'f':
-printf("%f", va_arg(ls, double));
-break;
-case 's':
-str = va_arg(ls, char *);
-if (str == NULL)
-str = "(nil)";
-
-printf("%s", str);
-break;
-}
-j = i + 1;
-
-while (format[j] && (format[j] == 'c' || format[j] == 'i' ||
-format[j] == 'f' || format[j] == 's'))
-{
-printf(", ");
-break;
-}
-
-i++;
-}
-printf("\n");
-va_end(ls);
->>>>>>> a699bf20910919295470c295867267b5b6ea4564
 }
