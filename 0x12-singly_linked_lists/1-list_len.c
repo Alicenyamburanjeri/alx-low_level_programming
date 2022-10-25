004a548be711af6cda286c1c52cd65fa0eb5f5fa
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2022-10-25 22:56:24 +0300

    list num

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,22 +2,17 @@
 #include "lists.h"
 
 /**
- * print_list - Prints length and the string, then returns amount of nodes
+ * list_len - Prints length and the string, then returns amount of nodes
  * @h: Pointer to a struct constant
  * Return: Returns amount of node
 **/
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 unsigned int count = 0;
 
 while (h != NULL)
 {
-if (h->str == NULL)
-printf("[%u] %s\n", 0, "(nil)");
-else
-printf("[%u] %s\n", h->len, h->str);
-
 h = h->next;
 count++;
 }
