17da5c6b3da33981bdc1dfa7943ab8242e9d1731
Alicenyamburanjeri <alicenyamburanjeri@gmail.com>
2023-04-12 13:27:44 -0700

    search algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,6 +1,20 @@
 #include "search_algos.h"
 #include <math.h>
 
+size_t min(size_t a, size_t b);
+
+/**
+ * min - returns the minimum of two size_t values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: `a` if lower or equal to `b`, `b` otherwise
+ */
+size_t min(size_t a, size_t b)
+{
+	return (a <= b ? a : b);
+}
+
 /**
  * jump_search - searches for a value in a sorted array of integers using
  * a jump search algorithm
